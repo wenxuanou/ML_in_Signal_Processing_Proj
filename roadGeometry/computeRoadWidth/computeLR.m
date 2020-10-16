920ac7a2b5363ed63a67b093445711b6374b6fe1
Prasad N R <PrasadNR@users.noreply.github.com>
2020-10-16 15:25:18 -0400

    Successfully parsed road geometry

--- roadGeometry/computeRoadWidth/computeLR.m
+++ roadGeometry/computeRoadWidth/computeLR.m
@@ -7,6 +7,12 @@ for i = 3:length(CSVfiles)
     CSVfileName = CSVfiles(i).name;
     CSVfile = fullfile(roadCSVfolder, CSVfileName);
     T = readtable(CSVfile);
-    X = T.x; Y = T.y;
-    figure; scatter(X, Y); title(CSVfileName);
+    X = T.x; Y = T.y; Cn = T.nodeID; Cw = T.way;
+    figure; 
+    scatter(X, Y);
+    title(CSVfileName);
+    
+    for j = 1:length(Cn)
+        text(X(j), Y(j), strcat(num2str(Cw(j)), "\_", num2str(Cn(j))));
+    end
 end
