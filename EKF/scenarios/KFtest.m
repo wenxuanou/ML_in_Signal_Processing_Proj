1b8728e23b1b4f9b1146e40d8be88a2014819b44
Prasad N R <PrasadNR@users.noreply.github.com>
2020-12-03 11:06:21 -0500

    Refactored KF code

---
+++ EKF/scenarios/KFtest.m
@@ -0,0 +1,17 @@
+clear; clc;
+Nobservations = 100; N = 1000;
+Ainit = eye(2); Binit = eye(2); Rinit = zeros(2, 2);
+s_ = zeros(Nobservations, 2);
+
+mu = [0 0]; Sigma = [0.01 0; 0 0.01]; R1 = chol(Sigma);
+e = repmat(mu, Nobservations, 1) + randn(Nobservations, 2) * R1;
+Gamma = repmat(mu, Nobservations, 1) + randn(Nobservations, 2) * R1;
+
+muE = mu; muGamma = mu;
+varE = Sigma; varGamma = Sigma;
+
+o_ = repmat(1:Nobservations, 2, 1)' * Binit + Gamma;
+
+sInit = s_'; o = o_';
+s = KF(sInit, N, Nobservations, o, muE, muGamma, varE, varGamma, Ainit, Binit, Rinit);
+plot(s(1, :), s(2, :));
