aa280e0bae10a5e81cc4f6b844aec9f6ca38ec95
Prasad N R <PrasadNR@users.noreply.github.com>
2020-12-05 00:43:54 -0500

    LKF is working!

--- EKF/scenarios/scenario1.m
+++ EKF/scenarios/scenario1.m
@@ -1,7 +1,7 @@
 clear; clc;
 
-N = 10;
-a1 = ones(1, N); a2 = -ones(1, N);
+N = 50;
+a1 = 10 * ones(1, N); a2 = -10 * ones(1, N);
 t1 = 0:(N - 1); t2 = 0:(N - 1);
 
 v1 = a1 .* t1; v2 = v1(end) + a2 .* t2;
@@ -11,6 +11,7 @@ x2 = x1(end) + v1(end) .* t2 + (0.5 * a2) .* (t2 .^ 2);
 x = [x1 x2]; v = [v1 v2]; a = [a1 a2];
 sOriginal = [x; v; a]; t = [t1 t2 + N];
 B = eye(3); A = eye(3); R = zeros(size(A)); I = eye(3);
+A = [1 1 0.5; 0 1 1; 0 0 1];
 
 Nobservations = 2 * N;
 o = B * sOriginal + normrnd(0, 1, size(R, 1), Nobservations);
@@ -19,8 +20,9 @@ muE = zeros(1, 3); muGamma = zeros(1, 3);
 varE = eye(3); varGamma = eye(3);
 sEstimated = KF(sInit, Nobservations, o, muE, muGamma, varE, varGamma, A, B, R);
 
-scatter(t, o(3,:))
+stateIndex = 1;
+scatter(t, o(stateIndex,:))
 hold on
-scatter(t, sEstimated(3, :))
+scatter(t, sEstimated(stateIndex, :))
 legend('observation', 'prediction')
 hold off
