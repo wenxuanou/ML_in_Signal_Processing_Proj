730d759d127696cd85dc295b2638186a9fcf959a
Rutaihwa <rutaihwa@gmail.com>
2020-12-06 17:02:47 +0100

    Starting to look forward N steps

---
+++ EKF/lookAhead.m
@@ -0,0 +1,34 @@
+function s = lookAhead(sInit, N, o, muE, muGamma, varE, varGamma, Ainit, Binit, Rinit)
+    % This function looks forward N steps, from a specic observation and state.
+
+    A = Ainit;
+    B = Binit; 
+    R = Rinit;
+    
+    s = zeros(size(sInit, 1), N);
+    s(:, 1) = sInit;
+
+    oBar = zeros(size(sInit, 1), N);
+    % Obar starts with first observation
+    oBar(:, 1) = o(:, 1);
+    
+     for i = 1:N
+         
+         % What is the previous state?
+         prev  = 1;
+         if i > 1
+             prev = i - 1;
+         end
+         
+         % Estimate the state for a given observation and confidence level
+         [estS, R ]= estimate(s(:, prev), oBar(:, prev), muE, muGamma, varE, varGamma, A, B, R);
+         
+         % Update state estimates and observation estimates
+         s(:, i) = estS;
+
+         % We need to define B, at the moment we use A.
+         oBar(:, i) = A * estS + muGamma';
+    
+     end
+
+end
---
+++ EKF/scenarios/scenario1_peek.m
@@ -0,0 +1,24 @@
+clear; clc;
+
+N = 50; deltaT = 1;
+a1 = 10 * ones(1, N); a2 = -10 * ones(1, N);
+t1 = 0:(N - 1); t2 = 0:(N - 1);
+
+a = [a1 a2];
+[a, v, x] = stateGenerator1D(a, deltaT);
+sOriginal = [x; v; a]; t = [t1 t2 + N];
+B = eye(3); A = [1 deltaT (deltaT^2)/2; 0 1 deltaT; 0 0 1]; R = zeros(size(A));
+
+Nobservations = 2 * N;
+o = B * sOriginal + normrnd(0, 1, size(R, 1), Nobservations);
+sInit = zeros(size(sOriginal, 1), 1);
+muE = zeros(1, 3); muGamma = zeros(1, 3);
+varE = eye(3); varGamma = eye(3);
+sEstimated = lookAhead(sInit, Nobservations, o, muE, muGamma, varE, varGamma, A, B, R);
+
+stateIndex = 1;
+scatter(t, o(stateIndex,:))
+hold on
+scatter(t, sEstimated(stateIndex, :))
+legend('observation', 'prediction')
+hold off
