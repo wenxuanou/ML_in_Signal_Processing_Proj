81026d1d18ff7c878c1b5220fedd917b1b2794eb
Prasad N R <PrasadNR@users.noreply.github.com>
2020-12-07 03:33:31 -0500

    KF working completely; Tuning might be needed!

--- EKF/KFControl/Teamwork.m
+++ EKF/KFControl/Teamwork.m
@@ -9,14 +9,14 @@ obstacleX = 50;
 obstacleY = 15;
 
 deltaT = 1;
-N = 100;        % number of time steps
+N = 100; % Number of time steps
 
 % initialize kalman filter
 ax = 1; % 0 or 1 or -1
 ay = 0; % 0 or 1 or -1
-u0 = [ax;ay];    % initialize driving term, 2*1
+uInit = [ax;ay]; % Initialize driving term, 2*1
 
-sInit = [carInitX;carInitY;0;0;ax;ay]; % s: [x;y;vx;vy;ax;ay]
+sInit = [carInitX; carInitY; 0; 0; ax; ay]; % s: [x; y; vx; vy; ax; ay]
 
 muE = zeros(1, 6); muGamma = zeros(1, 6);
 varE = eye(6); varGamma = eye(6);
@@ -40,9 +40,87 @@ B = eye(6); R = zeros(size(A));
 % training kalman filter
 % TODO: find o
 o = B * sInit + normrnd(0, 1, size(sInit));
-s = KFControl(sInit, N, o, muE, muGamma, varE, varGamma, A, B, R, G, u0, obstacleX, obstacleY, destinationX, destinationY);
+s = KFControlInline(sInit, N, o, muE, muGamma, varE, varGamma, A, B, R, G, uInit, obstacleX, obstacleY, destinationX, destinationY);
 
 figure(1)
 scatter(s(1,:),s(2,:))
 hold on
 scatter(obstacleX, obstacleY)
+
+function s = KFControlInline(sInit, N, o, muE, muGamma, varE, varGamma, A, B, R, G, uInit, obstacleX, obstacleY, destinationX, destinationY)
+    s = zeros(size(sInit, 1), N);
+    s(:, 1) = sInit; 
+
+    u = uInit;
+    for i = 2:N % Iterate over all observations
+        u = optimalControl2Inline(s(:, i-1),o,muE, muGamma, varE, varGamma, A, B, R, G, u, obstacleX, obstacleY, destinationX, destinationY);           % predict multiple step for optimal u
+
+        disp(u)
+        % Estimate the state for a given observation and confidence level
+        [estS, R ]= KFControlEstimateInline(s(:, i-1), o, muE, muGamma, varE, varGamma, A, B, R, G, u);    % move onstep forward
+        o  = B * estS;% + normrnd(0, 1, size(sInit));
+        s(:, i) = estS;
+
+    end
+
+end
+
+function [sHat, R]= KFControlEstimateInline(sPrev, o, muE, muGamma, varE, varGamma, A, B, R, G, u)
+% This fuction estimate a single state
+% Takes an single observation and estimate a state
+% Returns the predicted state and the confidence level
+    
+    I = eye(size(A));
+    sHat = sPrev;
+    
+    %Predict
+    % sT: [x;y;vx;vy;ax;ay], 6*1
+    sT = A * sHat + G * u + muE';   % st[N*1] = At[N*N] * st-1[N*1] + G[N*2] * u[2*1] + epsilon
+    R = varE + A * R * A';
+
+    %Update
+    K = R * B' * inv(B * R * B' + varGamma);
+    sHat = sT + K * (o - B * sT - muGamma');
+    R = (I - K * B) * R;
+    
+end
+
+function u_opt = optimalControl2Inline(si,oi,muE, muGamma, varE, varGamma, A, B, R, G, u, obstacleX, obstacleY, destinationX, destinationY)
+    ux = 0.7 * [0 0 0 0; 1 1 1 1; 0 0 0 0; -1 -1 -1 -1; 0 0 0 0; 1 1 -1 -1; 0 0 0 0; -1 -1 1 1; 0 0 0 0];
+    uy = 0.5 * [0 0 0 0; 0 0 0 0; 1 1 1 1; 0 0 0 0; -1 -1 -1 -1; 0 0 0 0; 1 1 -1 -1; 0 0 0 0; -1 -1 1 1];
+
+    muGaussianCost = 0; sigmaGaussianCost = 1;
+    
+     minCost = Inf;
+    u_opt = u;
+    for i = 1:size(ux, 1)
+        for j = 1:size(ux,2)
+            u = [ux(i,j);uy(i,j)];
+           
+            estS = si;
+            o_pred = oi;
+            cost = 0;
+            [estS,R]= KFControlEstimate(estS, o_pred, muE, muGamma, varE, varGamma, A, B, R, G, u);
+            o_pred = B * estS;% + normrnd(0, 1, size(oi));   % Number of state variable * 1
+
+            % observation:[x,y,vx,vy,ax,ay]
+            % accumulate cost alone the look ahead states
+            %cost = cost +...
+                %sqrt((o_pred(1) - destinationX)^2 + (o_pred(2) - destinationY)^2) +... 
+                %1/(sqrt((o_pred(1) - obstacleX)^2 + (o_pred(2) - obstacleY)^2));
+            
+            cost = cost + ...
+                sqrt((o_pred(1) - destinationX)^2 + (o_pred(2) - destinationY)^2) + ...
+                ((normpdf(o_pred(1) - obstacleX, muGaussianCost, sigmaGaussianCost) / normpdf(0, muGaussianCost, sigmaGaussianCost)) * ...
+                (normpdf(o_pred(2) - obstacleY, muGaussianCost, sigmaGaussianCost) / normpdf(0, muGaussianCost, sigmaGaussianCost)));
+                
+            if cost < minCost
+                u_opt = u;
+                minCost = cost;
+            end
+            
+        end
+    end
+    
+    disp(minCost)
+end
