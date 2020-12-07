ba76412702597c59f186b70ab938bab9d58d600c
Rutaihwa <rutaihwa@gmail.com>
2020-12-07 21:28:23 +0100

    Adding a documentation

--- simulator/straightDrive.m
+++ simulator/straightDrive.m
@@ -1,25 +1,32 @@
+%% striaghtDrive
+% Plot a movement of a car along a defined lenght of a road with no 
+% obstacle.
+%
+% parameters:
+% road_legth (some numbers), car_speed (m/s), car_positions(2 dimension * N )
+% Usage: straightDrive(50, 20, [0 2; 25 2 ])
+
 function [] = straightDrive(road_length, car_speed, car_positions)
 
-% A simple scenario
-scenario = drivingScenario;
+    % A driving scenario
+    scenario = drivingScenario;
 
-% Adding a 2 lanes road starting from 0 to road_leghth
-roadCenters = [0 0; road_length 0];
-road(scenario,roadCenters,'Lanes',lanespec(2));
+    % Adding a 2 lanes road starting from 0 to road_leghth
+    roadCenters = [0 0; road_length 0];
+    road(scenario,roadCenters,'Lanes',lanespec(2));
 
-% A car
-v = vehicle(scenario,'ClassID',1);
+    % A car
+    v = vehicle(scenario,'ClassID',1);
 
-% Making a trajectory
-trajectory(v,car_positions,car_speed)
+    % Making a trajectory
+    trajectory(v,car_positions,car_speed)
 
-% Plot the scenario and advance the vehicle
-plot(scenario)
+    % Plot the scenario and advance the vehicle
+    plot(scenario)
 
-% How fast are we plotting
-while advance(scenario)
-    pause(0.01)
-end
+    % How fast are we plotting
+    while advance(scenario)
+        pause(0.01)
+    end
 
-% if we want it to appear in the drivingScenario
 end
